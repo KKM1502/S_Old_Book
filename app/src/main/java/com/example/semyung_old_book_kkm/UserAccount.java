745478db966c46fead651809c614d85541c6ef67
unknown <gyeong112@gmail.com>
2022-04-05 15:25:48 +0900

    first subject

---
+++ app/src/main/java/com/example/semyung_old_book_kkm/UserAccount.java
@@ -0,0 +1,37 @@
+package com.example.semyung_old_book_kkm;
+
+/**
+ * 사용자 계정 모뎀 클래스
+ */
+public class UserAccount {
+
+    private String IdToken;     //Firebase Uid (고유아이디)
+    private String emailId;     //이메일
+    private String password;    //비밀번호
+
+    public String getIdToken() {
+        return IdToken;
+    }
+
+    public void setIdToken(String idToken) {
+        IdToken = idToken;
+    }
+
+    public UserAccount() { }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/semyung_old_book_kkm/activity_register.java
@@ -0,0 +1,71 @@
+package com.example.semyung_old_book_kkm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class activity_register extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef;
+    private EditText mEtEmail, mEtPwd;
+    private Button mBtnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("S_OldBook");
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegister = findViewById(R.id.btn_register);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 회원가입 처리
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                // FirebaseAuth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(activity_register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+
+                            Toast.makeText(activity_register.this,"회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
+
+                    } else{
+                            Toast.makeText(activity_register.this,"회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
+                        }
+                };
+            });
+        }
+
+        });
+
+    }
+}
