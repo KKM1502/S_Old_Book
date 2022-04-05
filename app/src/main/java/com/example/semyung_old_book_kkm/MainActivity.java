f6e16ef501e50c7ddf17b6ad727aa6622192c011
KKM1502 <102860627+KKM1502@users.noreply.github.com>
2022-04-05 18:30:14 +0900

    하단바

--- app/src/main/java/com/example/semyung_old_book_kkm/MainActivity.java
+++ app/src/main/java/com/example/semyung_old_book_kkm/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.semyung_old_book_kkm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView; //바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_menu:
+                        setFrag(0);
+                        break;
+                    case R.id.action_like:
+                        setFrag(1);
+                        break;
+                    case R.id.action_chat:
+                        setFrag(2);
+                        break;
+                    case R.id.action_account:
+                        setFrag(3);
+                        break;
+                }
+                return true;
+            }
+        });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        setFrag(0); // 첫 fragment 화면 지정할 것인지 선택.
+
+
     }
+
+    //fragment 교체가 일어나는 실행문
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+
+        }
+    }
+
+
+
 }
